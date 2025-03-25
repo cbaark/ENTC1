3a74e01651f6a74c7174beb98f681d191faad20d
denish <95339690+cbaark@users.noreply.github.com>
2025-03-25 17:54:01 +1100

    image slideshow, navbar fixes
    animations and js also added.

--- home.js
+++ home.js
@@ -1,19 +1,41 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+let slideTimer;
+showSlides(slideIndex);
 
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  resetSlideTimer();
 }
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  resetSlideTimer();
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active");
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].classList.add("active");
+  dots[slideIndex-1].className += " active";
+}
+
+function resetSlideTimer() {
+  clearTimeout(slideTimer);
+  slideTimer = setTimeout(autoAdvanceSlide, 5000); // Reset to 5 seconds
+}
+
+function autoAdvanceSlide() {
+  plusSlides(1);
+}
+
+// Start the initial timer
+resetSlideTimer();
